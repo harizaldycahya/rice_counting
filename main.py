499b0f51d56a54aa27bc071a9ba2be00e0ef5000
Dodoko <47654458+harizaldycahya@users.noreply.github.com>
2022-10-14 01:19:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     # parser.add_argument('--img-size', type=int, default=640,
     #                     help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float,
-                        default=0.537, help='object confidence threshold')
+                        default=0.503, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float,
                         default=0.5, help='IOU threshold for NMS')
     parser.add_argument('--device', default='',
